fd1582350c03a8efd1af918524a273d8b5f0087f
saifmalik2004 <saifmalik7827@gmail.com>
2024-10-18 17:49:10 +0530

    edit

--- day-4/practiceQuestions/Calculator.java
+++ day-4/practiceQuestions/Calculator.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 public class Calculator {
     public static void calculator(double num1, double num2, char operator) {
         double result;
-        if (operator == '+') {
-            result = num1 + num2;
-        } else if (operator == '-') {
-            result = num1 - num2;
-        } else if (operator == '*') {
-            result = num1 * num2;
-        } else if (operator == '/') {
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                System.out.println("Cannot divide by zero.");
-                return;
+
+        switch (operator) {
+            case '+' -> {
+                result = num1 + num2;
+                System.out.println("The result is: " + result);
             }
-        } else {
-            System.out.println("Invalid operator.");
-            return;
+            case '-' -> {
+                result = num1 - num2;
+                System.out.println("The result is: " + result);
+            }
+            case '*' -> {
+                result = num1 * num2;
+                System.out.println("The result is: " + result);
+            }
+            case '/' -> {
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("The result is: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+            }
+            default -> System.out.println("Invalid operator.");
         }
-        System.out.println("The result is: " + result);
     }
 
     public static void main(String[] args) {
--- day-4/practiceQuestions/CheckPassOrFail.java
+++ day-4/practiceQuestions/CheckPassOrFail.java
@@ -3,20 +3,11 @@ import java.util.Scanner;
 public class CheckPassOrFail {
     public static void checkPassOrFail(int marks) {
         switch (marks / 10) {
-            case 9: case 10:
-                System.out.println("Grade: A");
-                break;
-            case 8:
-                System.out.println("Grade: B");
-                break;
-            case 7:
-                System.out.println("Grade: C");
-                break;
-            case 6:
-                System.out.println("Grade: D");
-                break;
-            default:
-                System.out.println("Fail");
+            case 10, 9 -> System.out.println("Grade: A");
+            case 8 -> System.out.println("Grade: B");
+            case 7 -> System.out.println("Grade: C");
+            case 6 -> System.out.println("Grade: D");
+            default -> System.out.println("Fail");
         }
     }
 
