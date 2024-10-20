27ea88e6441a5d421c873a428853f089a5a21f22
saifmalik2004 <saifmalik7827@gmail.com>
2024-10-20 17:58:44 +0530

    update

--- day-6/practiceQuestions/LeapYearCheck.java
+++ day-6/practiceQuestions/LeapYearCheck.java
@@ -5,11 +5,7 @@ public class LeapYearCheck {
     // Function to check if a year is a leap year
     public static boolean isLeapYear(int year) {
         // A leap year is divisible by 4, but not divisible by 100 unless divisible by 400
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
     public static void main(String[] args) {
