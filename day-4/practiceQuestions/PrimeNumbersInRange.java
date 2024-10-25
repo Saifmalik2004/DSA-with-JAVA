070893826c0a200e515608f9f52ed55dd682759c
saifmalik2004 <saifmalik7827@gmail.com>
2024-10-25 21:59:42 +0530

    edit

--- day-4/practiceQuestions/PrimeNumbersInRange.java
+++ day-4/practiceQuestions/PrimeNumbersInRange.java
@@ -1,36 +1,39 @@
 import java.util.Scanner;
 
 public class PrimeNumbersInRange {
-
-    // Function to check if a number is prime
-    public static boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    // Function to print all prime numbers in the range from 1 to end
-    public static void primeNumbersInRange(int end) {
-        System.out.println("Prime numbers between 1 and " + end + " are:");
-        for (int i = 1; i <= end; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the ending number of the range (starting from 1):");
-        int end = sc.nextInt();
-
-        primeNumbersInRange(end);
-        sc.close();
+        // Prompt the user to enter the upper limit for the range
+        try ( // Create a Scanner object to read input from the user
+                Scanner sc = new Scanner(System.in)) {
+            // Prompt the user to enter the upper limit for the range
+            System.out.println("Enter the ending number of the range (starting from 1):");
+            int end = sc.nextInt();
+            // Display a message showing the range of prime numbers that will be printed
+            System.out.println("Prime numbers between 1 and " + end + " are:");
+            // Loop through each number from 1 to the user-defined end
+            for (int i = 1; i <= end; i++) {
+                boolean isPrime = true; // Assume the current number i is prime
+                
+                // Check if i is less than 2, which is not a prime number
+                if (i < 2) {
+                    isPrime = false;
+                } else {
+                    // Loop from 2 to half of i to check if any number divides i
+                    for (int j = 2; j <= i / 2; j++) {
+                        // If i is divisible by j, it is not prime
+                        if (i % j == 0) {
+                            isPrime = false; // Set isPrime to false if a divisor is found
+                            break; // Exit the loop since we found a divisor
+                        }
+                    }
+                }
+                
+                // If isPrime is still true, i is a prime number
+                if (isPrime) {
+                    System.out.println(i); // Print the prime number
+                }
+            }
+           
+        }
     }
 }
