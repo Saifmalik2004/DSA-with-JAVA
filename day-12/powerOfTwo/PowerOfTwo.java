ebd404ff2b54865f5a39cbd0380139dbe069c4a2
saifmalik2004 <saifmalik7827@gmail.com>
2024-11-07 14:54:37 +0530

    edit

--- day-12/powerOfTwo/PowerOfTwo.java
+++ day-12/powerOfTwo/PowerOfTwo.java
@@ -5,7 +5,7 @@ public class PowerOfTwo {
         // If n is a power of 2, it will have exactly one bit set to 1.
         // Example: 4 -> 100, 8 -> 1000, 16 -> 10000
         // n & (n-1) will be 0 if n is a power of 2.
-        return (n & (n - 1)) == 0;
+        return n>0 && (n & (31)) == 0;
     }
 
     public static void main(String[] args) {
