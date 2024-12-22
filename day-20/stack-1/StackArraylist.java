c153048299de10cd6e949c819bb577661d6895fa
saifmalik2004 <saifmalik7827@gmail.com>
2024-12-22 14:34:12 +0530

    comment added

--- day-20/stack-1/StackArraylist.java
+++ day-20/stack-1/StackArraylist.java
@@ -1,51 +1,48 @@
-
 import java.util.ArrayList;
 
-public class StackArraylist{
-    static class Stack  {
+public class StackArraylist {
+    static class Stack {
         static ArrayList<Integer> list = new ArrayList<>();
 
-        public  boolean  isEmpty() {
-             return list.isEmpty();
-            
+        // Check if the stack is empty
+        public boolean isEmpty() {
+            return list.isEmpty();
         }
 
-        public  void push(int data){
-            
+        // Push an element onto the stack
+        public void push(int data) {
             list.add(data);
         }
-        public  int pop(){
-            if(isEmpty()){
-                return -1;
+
+        // Pop the top element from the stack
+        public int pop() {
+            if (isEmpty()) {
+                return -1; // Return -1 if the stack is empty
             }
-            int top = list.get(list.size()-1);
-            list.remove(list.size()-1);
+            int top = list.get(list.size() - 1); // Get the top element
+            list.remove(list.size() - 1); // Remove it from the stack
             return top;
         }
 
-        public  int peek(){
-            if(isEmpty()){
-                return -1;
+        // Peek at the top element without removing it
+        public int peek() {
+            if (isEmpty()) {
+                return -1; // Return -1 if the stack is empty
             }
-            return list.get(list.size()-1);
-
+            return list.get(list.size() - 1); // Return the top element
         }
-
-        
-
     }
 
-
     public static void main(String[] args) {
-        Stack s=new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        Stack stack = new Stack();
+        stack.push(1); // Push 1 onto the stack
+        stack.push(2); // Push 2 onto the stack
+        stack.push(3); // Push 3 onto the stack
 
-        while(!s.isEmpty()){
-           System.out.println(s.peek());
-           s.pop();
+        // Print and pop elements from the stack
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek()); // Peek the top element
+            stack.pop(); // Pop the top element
         }
-       
     }
 }
