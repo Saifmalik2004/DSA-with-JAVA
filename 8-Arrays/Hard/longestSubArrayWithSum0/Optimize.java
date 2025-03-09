46be816953be269341dad177b3cb04ace2bc64e8
saifmalik2004 <saifmalik7827@gmail.com>
2025-03-09 16:01:52 +0530

    longest subarray with sum 0

--- 8-Arrays/Hard/longestSubArrayWithSum0/Optimize.java
+++ 8-Arrays/Hard/longestSubArrayWithSum0/Optimize.java
@@ -1,38 +1,40 @@
-
 import java.util.HashMap;
 
-
 public class Optimize {
-    public static int find(int arr[],int n) {
+
+    // Function to find the maximum length of subarray with sum 0
+    public static int find(int arr[], int n) {
+        // HashMap to store the sum at each index
         HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
 
-        int maxi = 0;
-        int sum = 0; 
+        int maxi = 0;  // Variable to track the maximum length of subarray
+        int sum = 0;   // Variable to track the prefix sum
 
-        for(int i = 0;i<n;i++) {
+        // Loop through the array
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];  // Add current element to the sum
 
-            sum += arr[i]; 
-
-            if(sum == 0) {
-                maxi = i + 1; 
-            }
-            else {
-                if(mpp.get(sum) != null) {
-
-                    maxi = Math.max(maxi, i - mpp.get(sum)); 
-                }
-                else {
-
-                    mpp.put(sum, i); 
+            // If the sum is 0, it means the subarray from the start to the current index has a sum 0
+            if (sum == 0) {
+                maxi = i + 1;  // Update maxi to current index + 1
+            } else {
+                // If the sum has been encountered before, it means there is a subarray between previous index and current index with sum 0
+                if (mpp.get(sum) != null) {
+                    maxi = Math.max(maxi, i - mpp.get(sum));  // Calculate length of subarray and update maxi
+                } else {
+                    // Otherwise, store the index of the first occurrence of the sum
+                    mpp.put(sum, i);
                 }
             }
         }
-        return maxi; 
+        return maxi;  // Return the maximum length of subarray with sum 0
     }
-    public static void main(String[] args) {
-        int a[]={1,-1,0,0,2,3,-3,0,1};
 
-       
-        System.out.println( find(a, a.length));
-     }
+    public static void main(String[] args) {
+        // Sample input array
+        int a[] = {1, -1, 0, 0, 2, 3, -3, 0, 1};
+
+        // Output the result
+        System.out.println(find(a, a.length));  // Calling the function with the array and its length
+    }
 }
