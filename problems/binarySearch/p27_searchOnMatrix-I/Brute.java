86cd6596358c8604a74155985ef218fe37211d31
saifmalik2004 <saifmalik7827@gmail.com>
2025-07-30 16:05:04 +0530

    p27 search on sorted matrix

---
+++ problems/binarySearch/p27_searchOnMatrix-I/Brute.java
@@ -0,0 +1,30 @@
+public class Brute {
+    public static boolean search(int[][] matrix,int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j]==k) return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Matrix:");
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Brute Search 4: " + search(matrix, 4));
+    }
+}
