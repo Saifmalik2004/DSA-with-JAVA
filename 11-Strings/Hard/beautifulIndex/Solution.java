d0bd246ec5d8327d4d80eca0669f97805f6f8a28
saifmalik2004 <saifmalik7827@gmail.com>
2025-08-15 17:01:33 +0530

    beautiful index

---
+++ 11-Strings/Hard/beautifulIndex/Solution.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> beautifulIndices(String s, String a, String b, int k) {
+        char[] sChar = s.toCharArray();
+        List<Integer> indicesA = kmp(sChar, a.toCharArray());
+        List<Integer> indicesB = kmp(sChar, b.toCharArray());
+        List<Integer> res = new ArrayList<>();
+        int j = 0;
+        
+        for (int i = 0; i < indicesA.size(); i++) {
+            boolean found = false;
+            int aIndex = indicesA.get(i);
+
+            // Check left side
+            while (j < indicesB.size() && aIndex > indicesB.get(j)) {
+                if (aIndex - indicesB.get(j) <= k) {
+                    res.add(aIndex);
+                    found = true;
+                    break;
+                }
+                j++;
+            }
+
+            // Check right side
+            int lowerBound = j;
+            while (!found && lowerBound < indicesB.size() && indicesB.get(lowerBound) <= aIndex + k) {
+                if (indicesB.get(lowerBound) - aIndex <= k) {
+                    res.add(aIndex);
+                    found = true;
+                }
+                lowerBound++;
+            }
+        }
+
+        return res;
+    }
+
+    private List<Integer> kmp(char[] s, char[] pattern) {
+        int[] lsp = computeLsp(pattern);
+        List<Integer> res = new ArrayList<>();
+        int j = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            while (j > 0 && s[i] != pattern[j]) {
+                j = lsp[j - 1];
+            }
+            if (s[i] == pattern[j]) {
+                j++;
+            }
+            if (j == pattern.length) {
+                res.add(i - j + 1);
+                j = lsp[j - 1];
+            }
+        }
+        return res;
+    }
+
+    private int[] computeLsp(char[] s) {
+        int length = 0;
+        int i = 1;
+        int[] lsp = new int[s.length];
+
+        while (i < s.length) {
+            while (length > 0 && s[i] != s[length]) {
+                length = lsp[length - 1];
+            }
+            if (s[length] == s[i]) {
+                ++length;
+            }
+            lsp[i++] = length;
+        }
+        return lsp;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "isawsquirrelnearmysquirrelhouseohmy";
+        String a = "my";
+        String b = "squirrel";
+        int k = 15;
+
+        List<Integer> result = sol.beautifulIndices(s, a, b, k);
+        System.out.println(result);
+    }
+}
