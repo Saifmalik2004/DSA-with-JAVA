97d7254a6c1629bb7678c5b6b5ef5391d79627d2
saifmalik2004 <saifmalik7827@gmail.com>
2025-09-04 18:09:47 +0530

    find mid

---
+++ 19-Linkedlist/singlyLinkedList/Easy/4-findMid/LinkedList.java
@@ -0,0 +1,132 @@
+public class LinkedList {
+
+    // Node class
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private static int size;
+    private static Node head;
+
+    // Add node at the beginning
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head  = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add node at the end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Print LinkedList
+    public void print() {
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Remove first node
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LinkedList is empty");
+            return -1;
+        } else if (size == 1) {
+            int val = head.data;
+            head = null;
+            size--;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    // Remove last node
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LinkedList is empty");
+            return -1;
+        } else if (size == 1) {
+            int val = head.data;
+            head = null;
+            size--;
+            return val;
+        }
+
+        Node temp = head;
+        while (temp.next.next != null) { // stop at second-last node
+            temp = temp.next;
+        }
+
+        int val = temp.next.data;
+        temp.next = null;
+        size--;
+
+        return val;
+    }
+
+    // ✅ Find middle node using slow & fast pointers
+    public void findMiddle() {
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+            return;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("Middle element is: " + slow.data);
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
+        ll.print();  // 5 --> 4 --> 3 --> 2 --> 1 --> null
+
+        ll.findMiddle(); // Output: 3
+    }
+}
