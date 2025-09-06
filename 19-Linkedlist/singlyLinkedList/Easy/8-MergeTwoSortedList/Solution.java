1526969d5963394bdb384026236774d62598d351
saifmalik2004 <saifmalik7827@gmail.com>
2025-09-06 11:28:11 +0530

    merge two sorted list

---
+++ 19-Linkedlist/singlyLinkedList/Easy/8-MergeTwoSortedList/Solution.java
@@ -0,0 +1,63 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class Solution {
+   
+    // Method to merge two sorted lists
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        // Add remaining nodes
+        if (l1 != null) curr.next = l1;
+        if (l2 != null) curr.next = l2;
+
+        return dummy.next;
+    }
+
+    // Helper function to print LinkedList
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // Main method to test locally
+    public static void main(String[] args) {
+         // First sorted list: 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        // Second sorted list: 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        Solution sol = new Solution();
+        ListNode result = sol.mergeTwoLists(l1, l2);
+
+        printList(result);
+    }
+}
