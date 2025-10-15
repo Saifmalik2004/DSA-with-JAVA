dd82cd01c05712f4efc426ade238e36a94949e63
saifmalik2004 <saifmalik7827@gmail.com>
2025-10-15 19:53:07 +0530

    132 patern

---
+++ 20-Stack/medium/9-132Pattern/Solution.java
@@ -0,0 +1,20 @@
+
+import java.util.Stack;
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st= new Stack<>();
+        int third=Integer.MIN_VALUE;
+
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<third) return true;
+            while(!st.isEmpty() && st.peek()<nums[i]){
+                third=st.pop();
+            }
+
+            st.push(nums[i]);
+
+        }
+        return false;
+    }
+}
